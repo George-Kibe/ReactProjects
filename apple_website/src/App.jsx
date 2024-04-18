cbf99b219b29dc5c447b0f659de661985afa4b64
George-Kibe <georgekibewambui@gmail.com>
2024-04-18 16:47:28 +0300

    finalizing the project

--- apple_website/src/App.jsx
+++ apple_website/src/App.jsx
@@ -1,13 +1,23 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlighs from "./components/Highlights";
+import Model from "./components/Model";
+import Footer from "./components/Footer";
+import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
 
 function App() {
 
   return (
-    <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-
-    </>
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlighs />
+      <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
+    </main>
   )
 }
 

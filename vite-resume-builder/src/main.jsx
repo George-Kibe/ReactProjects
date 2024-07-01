85b4d366c929c396284c2398e97ef2e2387dd53b
George-Kibe <georgekibewambui@gmail.com>
2024-07-01 17:20:53 +0300

    react router dom

--- vite-resume-builder/src/main.jsx
+++ vite-resume-builder/src/main.jsx
@@ -2,9 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import SignInPage from './auth/sign-in'
+import HomePage from './screens/home'
+import Dashboard from './screens/dashboard'
+
+const router = createBrowserRouter([
+  {
+    // path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard />,
+      },
+    ],
+  },
+  {
+    path: '/auth/sign-in',
+    element: <SignInPage />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )

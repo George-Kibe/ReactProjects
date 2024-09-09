7e6d7ea9d9c7ad874bdc041e9f14d68f722274dd
George-Kibe <georgekibewambui@gmail.com>
2024-09-09 18:19:00 +0300

    hover-components

---
+++ hover-components/src/app/page.tsx
@@ -0,0 +1,29 @@
+"use client"
+import Image from "next/image";
+import {DarkGridAuth} from "@/components/AuthComponents";
+import {Kanban} from "@/components/Kanban";
+import FloatingPhone from "@/components/FloatingPhone";
+import {HoverImageLinks} from "@/components/HoverImageLinks";
+import {MouseImage} from "@/components/MouseImageTrail";
+import {Grids} from "@/components/Grids";
+import ShiftingCountdown from "@/components/CountDown";
+import {TextParallaxContentExample} from "@/components/TextParallaxContent";
+import {VanishList} from "@/components/VanishList";
+import {LogoOrigami} from "@/components/LogoOrigami";
+
+export default function Home() {
+  return (
+    <div>
+      {/* <DarkGridAuth /> */}
+      {/* <Kanban /> */}
+      {/* <FloatingPhone /> */}
+      {/* <HoverImageLinks /> */}
+      {/* <MouseImage /> */}
+      {/* <Grids /> */}
+      {/* <ShiftingCountdown /> */}
+      {/* <TextParallaxContentExample /> */}
+      {/* <VanishList /> */}
+      <LogoOrigami />
+    </div>
+  );
+}

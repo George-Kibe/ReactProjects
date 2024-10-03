a8a9c0f4d4f4bf32215e3dae6c4a253e6bfd3fe8
George-Kibe <georgekibewambui@gmail.com>
2024-10-03 22:14:39 +0300

    responsive navbar with sub menus

---
+++ nav-test/src/app/page.js
@@ -0,0 +1,10 @@
+import NavBarTwo from "@/components/NavBarTwo";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <NavBarTwo />
+    </div>
+  );
+}
